ac65c0e55a18bdb7122678bf1ffe4c36d0b4a39b
lEsoq <lolssdwsqs2@gmail.com>
2025-03-03 16:58:56 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,53 +1,16 @@
-//Toggling Menu
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId);
-
-    if(toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
-        })
-    }
-}
-
-showMenu('nav-toggle', 'nav-menu');
-
-//Toggling Active Link
-const navLink = document.querySelectorAll('.nav-link');
-
-function linkAction() {
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show');
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-// Scroll Reveal
-
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
-
-sr.reveal('.home-title', {} )
-sr.reveal('.button', {delay: 200} )
-sr.reveal('.home-img', {delay: 400} )
-sr.reveal('.home-social', {delay: 400,} )
-
-sr.reveal('.about-img', {} )
-sr.reveal('.about-subtitle', {delay: 200} )
-sr.reveal('.about-text', {delay: 400} )
-
-sr.reveal('.skills-subtitle', {delay: 100} )
-sr.reveal('.skills-text', {delay: 150} )
-sr.reveal('.skills-data', {interval: 200} )
-sr.reveal('.skills-img', {delay: 400} )
-
-sr.reveal('.work-img', {interval: 200} )
-
-sr.reveal('.contact-input', {interval: 200} )
+// Make sections visible when scrolling
+window.addEventListener('scroll', function () {
+    let sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      let sectionPosition = section.getBoundingClientRect().top;
+      if (sectionPosition <= window.innerHeight * 0.8) {
+        section.classList.add('visible');
+      }
+    });
+  });
+  
+  // Scroll to the section on click
+  function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
